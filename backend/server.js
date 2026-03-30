1d0d6292477495982ba7afca413d1d8eb923e844
digitalrehman <abdulrehmanarain1018@gmail.com>
2026-03-30 16:24:20 +0500

    feat: implement MongoDB connection configuration and initialize in server entry point

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,13 @@ import express from "express";
 import router from "./src/router/router.js";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import "dotenv/config"
 import { serve } from "inngest/express";
 import { inngest, functions } from "./src/inngest/index.js";
+import dbConnection from "./src/config/db.js";
 let app = express();
 let port = process.env.PORT || 3000;
-
+dbConnection()
 
 app.use(express.json());
 app.use(cors());
